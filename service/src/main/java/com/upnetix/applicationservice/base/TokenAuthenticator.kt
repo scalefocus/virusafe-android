82727de799e9709f1068ebfec75c16b63602dee1
stoyan.yanev <stoyan.yanev@upnetix.com>
2020-04-16 13:55:34 +0300

    Finished implementation for refresh token.

--- service/src/main/java/com/upnetix/applicationservice/base/TokenAuthenticator.kt
+++ service/src/main/java/com/upnetix/applicationservice/base/TokenAuthenticator.kt
@@ -1,8 +1,11 @@
 package com.upnetix.applicationservice.base
 
-import android.util.Log
+import com.upnetix.applicationservice.base.BaseService.Companion.INVALID_TOKEN
+import com.upnetix.applicationservice.base.HeaderInterceptor.Companion.AUTHORIZATION_KEY
+import com.upnetix.applicationservice.base.HeaderInterceptor.Companion.BEARER_KEY
 import com.upnetix.applicationservice.registration.IRegistrationService
 import dagger.Lazy
+import kotlinx.coroutines.runBlocking
 import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
@@ -10,8 +13,27 @@ import okhttp3.Route
 import javax.inject.Inject
 
 class TokenAuthenticator @Inject constructor(private val serviceWrapper: Lazy<IRegistrationService>) : Authenticator {
-	override fun authenticate(route: Route?, response: Response): Request? {
 
-		return response.request().newBuilder().build()
+	private var retryCount = 0;
+
+	override fun authenticate(route: Route?, response: Response): Request? = runBlocking {
+
+		if (retryCount > 1) {
+			throw AuthProtocolException(INVALID_TOKEN)
+		}
+		retryCount++
+
+		try {
+			when (val tokenResponse = serviceWrapper.get()?.refreshToken()) {
+				is ResponseWrapper.Success -> response.request().newBuilder().header(
+					AUTHORIZATION_KEY,
+					"$BEARER_KEY ${tokenResponse.response.accessToken}"
+				).build()
+				is ResponseWrapper.Error -> throw AuthProtocolException(tokenResponse.code)
+				else -> null
+			}
+		} finally {
+			retryCount = 0
+		}
 	}
 }
